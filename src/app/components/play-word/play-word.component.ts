7c5a17f99e2556baf3f8e8a0cb445c1059a6e611
MrManoloDG <frkwtf@gmail.com>
2023-04-16 21:53:39 +0200

    feat: added restart game and show real word

--- src/app/components/play-word/play-word.component.ts
+++ src/app/components/play-word/play-word.component.ts
@@ -1,16 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
+import { GameControllerService } from 'src/api/gameController.service';
 
 @Component({
   selector: 'app-play-word',
   templateUrl: './play-word.component.html',
   styleUrls: ['./play-word.component.scss']
 })
-export class PlayWordComponent {
+export class PlayWordComponent implements OnInit {
 
+  constructor(private activatedRoute: ActivatedRoute, private gameService: GameControllerService){}
+  word!: string;
   isRevealed = false;
-
+  uuid!: string;
+  user!: string;
+  gameData!: any;
+  owner!: string;
   switchRevealed(){
     this.isRevealed = !this.isRevealed;
+  }
+
+  async ngOnInit() {
+    this.user = sessionStorage.getItem('username') as unknown as string;
+    this.activatedRoute.paramMap.subscribe(async obs => {
+      this.uuid = obs.get('id') as unknown as string;
+      this.gameData = await lastValueFrom(this.gameService.getGame(this.uuid));
+      this.owner = this.gameData.createdBy;
+      await this.updateGameData();
+      setInterval(async () => {
+        await this.updateGameData();
+      }, 2000);
+    });
+
+  }
+
+  async updateGameData(){
+    const response = await lastValueFrom(this.gameService.getWord(this.uuid,this.user));
+    this.word = response;
+  }
+
+  async startGame() {
+    const response = await lastValueFrom(this.gameService.startGame(this.uuid, this.user));
+    console.log(response);
   }
 
 }
